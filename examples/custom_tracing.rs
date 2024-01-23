a2e7d85c99c789763cc959338a8494da95b5d5f1
0xKitsune <0xKitsune@protonmail.com>
2024-01-23 10:05:42 -0500

    updated example

--- examples/custom_tracing.rs
+++ examples/custom_tracing.rs
@@ -8,10 +8,8 @@ pub fn main() -> eyre::Result<()> {
     let datadog_layer =
         datadog_layer("datadog-example", "http://localhost:8126", true);
 
-    let stdout_layer = stdout_layer();
-
     tracing_subscriber::registry()
-        .with(stdout_layer)
+        .with(stdout_layer())
         .with(datadog_layer)
         .init();
 
