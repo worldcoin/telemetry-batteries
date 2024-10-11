901ea26e478c81e10d5d4355ac628ab7b15afca7
Ryan Butler <thebutlah@gmail.com>
2024-10-11 13:37:47 -0400

    reexport opentelemetry crate (#45)

--- telemetry-batteries/src/lib.rs
+++ telemetry-batteries/src/lib.rs
@@ -1,2 +1,11 @@
 pub mod metrics;
 pub mod tracing;
+
+/// Reexports of crates that appear in the public API.
+///
+/// Using these directly instead of adding them yourself to Cargo.toml will help avoid
+/// errors where types have the same name but actually are distinct types from different
+/// crate versions.
+pub mod reexports {
+    pub use ::opentelemetry;
+}
