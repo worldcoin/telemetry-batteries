8d34e6e42730c47a9166402c98a7688500a3cd41
Dzejkop <jakubtrad@gmail.com>
2026-06-23 15:32:33 +0200

    add tokio task panic to example

--- examples/panic_example.rs
+++ examples/panic_example.rs
@@ -4,11 +4,14 @@ use telemetry_batteries::TopLevelResultExt;
 async fn main() -> eyre::Result<()> {
     let _guard = telemetry_batteries::init()?;
 
+    let _ = tokio::spawn(async { panic!("this is a panic in a tokio task!") })
+        .await;
+
     run().await.panic_on_top_level_error();
 
     Ok(())
 }
 
 async fn run() -> eyre::Result<()> {
-    eyre::bail!("top level error! What will it look like!");
+    eyre::bail!("top level error!");
 }
